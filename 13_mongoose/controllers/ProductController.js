5b5451cbd1dc2cd13980f1e8cefd58c66bb96017
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-21 16:14:59 -0300

    feat: fnd product by id

--- 13_mongoose/controllers/ProductController.js
+++ 13_mongoose/controllers/ProductController.js
@@ -6,11 +6,11 @@ export const ProductController = class ProductController {
     res.render('products/all', { products })
   }
 
-  // static async showProduct(req, res) {
-  //   const { id } = req.params
-  //   const product = await Product.getProductById(id)
-  //   res.render('products/product', { product })
-  // }
+  static async showProduct(req, res) {
+    const { id } = req.params
+    const product = await Product.findById(id).lean()
+    res.render('products/product', { product })
+  }
 
   static createProduct(req, res) {
     res.render('products/create')
