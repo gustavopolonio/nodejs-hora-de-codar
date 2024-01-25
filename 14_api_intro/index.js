096e4e1912f5000ba7cabf26f88471a3eb26f7a0
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-25 17:19:29 -0300

    http status code

--- 14_api_intro/index.js
+++ 14_api_intro/index.js
@@ -5,15 +5,20 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'First route' })
-})
-
 app.post('/create-product', (req, res) => {
   const { name, price } = req.body
-  console.log(name, price)
+  // console.log(name, price)
 
-  res.json({ message: `Product ${name} was created!` })
+  if (!name) {
+    res.status(422).json({ message: 'Name field is required!' })
+    return
+  }
+
+  res.status(201).json({ message: `Product ${name} was created!` })
+})
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'First route' })
 })
 
 app.listen(3000)
