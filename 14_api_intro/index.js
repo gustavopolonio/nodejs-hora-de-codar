b3a5c52ca65fab8cfb2a8135deb685b0316de982
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-25 17:08:04 -0300

    creating post route

--- 14_api_intro/index.js
+++ 14_api_intro/index.js
@@ -9,4 +9,11 @@ app.get('/', (req, res) => {
   res.json({ message: 'First route' })
 })
 
+app.post('/create-product', (req, res) => {
+  const { name, price } = req.body
+  console.log(name, price)
+
+  res.json({ message: `Product ${name} was created!` })
+})
+
 app.listen(3000)
