ed59106bd821085c9cb0ff3bbc56da073a7dbe14
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-29 15:57:18 -0300

    feat: creating user routes schema

---
+++ 15_get_a_pet/backend/controllers/UserController.js
@@ -0,0 +1,7 @@
+import { User } from '../models/User.js'
+
+export class UserController {
+  static register(req, res) {
+    res.json('Hello Get a Pet :)')
+  }
+}
--- 15_get_a_pet/backend/index.js
+++ 15_get_a_pet/backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
+import { router as UserRoutes } from './routes/UserRoutes.js'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static('public'))
 
+app.use('/users', UserRoutes)
 
 app.listen(5000)
